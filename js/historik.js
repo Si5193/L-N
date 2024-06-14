eea615cbbc813910df70a2f0c7fb5846ea0d1e34
Si5193 <simonstrandlund@gmail.com>
2024-06-14 10:25:37 +0200

    Update historik.js

--- js/historik.js
+++ js/historik.js
@@ -8,16 +8,37 @@ async function fetchHistorik() {
     historikList.innerHTML = ''; // Töm listan först
     const q = query(collection(db, "revenues"), orderBy("date"));
     const querySnapshot = await getDocs(q);
+
+    const monthNames = ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"];
+    const monthlyData = {};
+
     querySnapshot.forEach((doc) => {
         const data = doc.data();
+        const date = new Date(data.date);
+        const month = date.getMonth();
+        const year = date.getFullYear();
+        const monthYear = `${year}-${month}`;
+
+        if (!monthlyData[monthYear]) {
+            monthlyData[monthYear] = { revenue: 0, provision: 0 };
+        }
+
+        monthlyData[monthYear].revenue += data.revenue;
+        if (data.provision) {
+            monthlyData[monthYear].provision += data.provision;
+        }
+    });
+
+    for (const [key, value] of Object.entries(monthlyData)) {
+        const [year, month] = key.split('-');
         const listItem = document.createElement('tr');
         listItem.innerHTML = `
-            <td>${data.date}</td>
-            <td>${data.revenue} kr</td>
-            <td>${data.provision ? data.provision + ' kr' : 'Ingen provision'}</td>
+            <td>${monthNames[month]} ${year}</td>
+            <td>${value.revenue} kr</td>
+            <td>${value.provision ? value.provision + ' kr' : 'Ingen provision'}</td>
         `;
         historikList.appendChild(listItem);
-    });
+    }
 }
 
 // Hämta data när sidan laddas
