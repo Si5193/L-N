d8b0e238682b62a9d5028a0a53381e7d86f7b53b
Si5193 <simonstrandlund@gmail.com>
2024-06-14 14:24:33 +0200

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyC0M-i21bF1HnjThFqacN9FJ8pUageNmP4",
     authDomain: "provsion1.firebaseapp.com",
@@ -14,7 +12,6 @@ const firebaseConfig = {
     measurementId: "G-W2REDG7RRC"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
