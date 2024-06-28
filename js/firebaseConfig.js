23b03f8ff0f18de2262e91ca34b5e4809d1b2e39
Si5193 <simonstrandlund@gmail.com>
2024-06-28 08:37:00 +0200

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyC0M-i21bF1HnjThFqacN9FJ8pUageNmP4",
@@ -12,8 +12,7 @@ const firebaseConfig = {
     measurementId: "G-W2REDG7RRC"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-
-export { db, auth };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
