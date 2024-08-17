bf2612b99c7d01861fa32be47fa25c12527b2f6d
Si5193 <simonstrandlund@gmail.com>
2024-08-17 22:32:22 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,29 +1,15 @@
-import { auth, db } from './firebaseConfig.js';
+import { auth } from './firebaseConfig.js';
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { query, where, getDocs, collection } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const loginForm = document.getElementById('loginForm');
 
 loginForm.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const username = document.getElementById('username').value;
+    const email = document.getElementById('username').value; // Användarnamn fältet blir nu e-post
     const password = document.getElementById('password').value;
 
     try {
-        const q = query(collection(db, "users"), where("username", "==", username));
-        const querySnapshot = await getDocs(q);
-
-        if (querySnapshot.empty) {
-            alert('Felaktigt användarnamn eller lösenord.');
-            return;
-        }
-
-        let email = '';
-        querySnapshot.forEach((doc) => {
-            email = doc.data().email;
-        });
-
         await signInWithEmailAndPassword(auth, email, password);
         alert('Inloggning lyckades!');
         window.location.href = 'dashboard.html';
