1c56f0e90e3510b0b2146b9845584b509e07dbbd
Si5193 <simonstrandlund@gmail.com>
2024-08-17 22:32:46 +0200

    Update register.js

--- js/register.js
+++ js/register.js
@@ -2,7 +2,7 @@ import { auth, db } from './firebaseConfig.js';
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const registerForm = document.getElementById('registerForm');
+const registerForm = document.getElementById('register-form');
 
 registerForm.addEventListener('submit', async (e) => {
     e.preventDefault();
